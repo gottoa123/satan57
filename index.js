21c31e3b39935d841034dbe54d4578bc17b4d1f1
gottoa123 <37652250+gottoa123@users.noreply.github.com>
2024-09-11 09:51:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json()); // ใช้การแปลง JSON ในตัวของ Express
 app.use(bodyParser.json());
 
-const LINE_ACCESS_TOKEN = process.env.LINE_ACCESS_TOKEN;
+const LINE_ACCESS_TOKEN = process.env.satan47_LINE_ACCESS_TOKEN;
 
 app.post("/webhook", async (req, res) => {
   console.log('Request body:', req.body); // ตรวจสอบข้อมูลที่ได้รับ
